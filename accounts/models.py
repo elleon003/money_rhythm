380f0d481dd078804d5473e41fcfdec5c764aa39
elleon003 <noelle@ygitc.com>
2025-01-28 21:56:10 -0500

    Initial setup

---
+++ accounts/models.py
@@ -0,0 +1,56 @@
+from django.contrib.auth.models import AbstractUser, PermissionMixin
+from django.db import models
+from django.utils import timezone
+from django.utls.translation import ugettext_lazy as _
+
+from .managers import CustomUserManager
+
+
+class User(AbstractUser, PermissionMixin):
+    email = models.EmailField(
+        _('email address'), 
+        unique=True,
+        error_messages={
+            'unique': _("A user with that email already exists."),
+        }
+    )
+
+    first_name = models.CharField(_('first name'), max_length=30, blank=True)
+    last_name = models.CharField(_('last name'), max_length=30, blank=True)
+    is_staff = models.BooleanField(
+        _('staff status'),
+        default=False,
+        help_text=_('Designates whether the user can log into this admin site.'),
+    )
+
+    is_active = models.BooleanField(
+        _('active'),
+        default=True,
+        help_text=_(
+            'Designates whether this user should be treated as active. '
+            'Unselect this instead of deleting accounts.'
+        ),
+    )
+
+    date_joined = models.DateTimeField(_('date joined'), default=timezone.now)
+
+    EMAIL_FIELD = 'email'
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
+    objects = CustomUserManager()
+
+    class Meta:
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
+
+    def get_full_name(self):
+        """
+        Return the first_name plus the last_name, with a space in between.
+        """
+        full_name = f'{self.first_name} {self.last_name}'
+        return full_name.strip()
+
+    def get_short_name(self):
+        """Return the short name for the user."""
+        return self.first_name
